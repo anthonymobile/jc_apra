585425e9430ec0fa7a972120b29e314ef42104c7
Anthony Townsend <anthony@starcitygroup.us>
2021-11-13 18:23:15 -0500

    heroku deploy BETA working

--- app.py
+++ app.py
@@ -252,9 +252,21 @@ folium_static(map)
 # NOTICE
 ###################################################################
 
-st.subheader('Where does this data come from?')
+import os
+import base64
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    return href
 
+
+
+st.subheader('Where does this data come from?')
 st.markdown('The Abandoned Property Rehabilitation Act (APRA), adopted by the State of New Jersey in 2004, requires municipalities to maintain a list of vacant and abandoned properties. The data on this site are collated from lists published by the City of Jersey City since 2014. We are currently awaiting 2019 and 2020 figures through an Open Public Records Act (OPRA) request. A copy of the collated, cleaned, and geocoded data can be downloaded here.')
+
+st.markdown(get_binary_file_downloader_html('./data/gdf_patched.xlsx', ' Data'), unsafe_allow_html=True)
 
 
 image = Image.open('./www/163-clerk-st.png')
